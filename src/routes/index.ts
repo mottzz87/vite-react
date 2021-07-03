ddca3a37f8c4f7191c63a44be1481189640b5f96
vane <hualuna87@126.com>
2021-07-04 06:26:48 +0800

    t

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Vane
  * @Date: 2021-07-03 22:43:24
- * @LastEditTime: 2021-07-04 05:22:04
+ * @LastEditTime: 2021-07-04 06:26:10
  * @LastEditors: Vane
  * @Description:
  * @FilePath: \vite-react\src\routes\index.ts
@@ -10,6 +10,7 @@
 import Index from '@/pages/Index';
 import About from '@/pages/About';
 
+const a = 1
 const routes = [
 	{ path: '/', component: Index },
 	{ path: '/about', component: About },
